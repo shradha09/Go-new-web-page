2ced1f59459cc47a1285f4fc3a07955be92dbd83
shradha09 <shradha09@users.noreply.github.com>
2016-03-11 09:38:54 +0530

    Update mongodb-data1.go

--- mongodb-data1.go
+++ mongodb-data1.go
@@ -31,17 +31,16 @@ if err != nil {
     session.SetMode(mgo.Monotonic, true)
 
     c := session.DB("testdb").C("people")
-    err = c.Insert(&Person{"Ale", "+55 53 8116 9639"},
-        &Person{"Cla", "+55 53 8402 8510"})
+    err = c.Insert(&Person{"Shradha", "+91 96 8116 9639"},
+        &Person{"Shivangi", "+91 87 8402 8510"})
     if err != nil {
         panic(err)
     }
 
     result := Person{}
-    err = c.Find(bson.M{"name": "Ale"}).One(&result)
+    err = c.Find(bson.M{"name": "Shradha"}).One(&result)
     if err != nil {
         panic(err)
     }
 
-fmt.Println("Phone:", result.Phone)
 }
